db162ce41978837a17febd6431310938924ab2fd
Hui Song <hui.song@sintef.no>
2016-08-17 15:13:04 +0100

    current model

---
+++ model.js
@@ -0,0 +1,127 @@
+var model = {
+    "n7": {
+        "title": "gap ",
+        "loc": "473@48"
+    },
+    "n12": {
+        "title": " low cost",
+        "loc": "809@44"
+    },
+    "n17": {
+        "title": " cloud trend",
+        "loc": "1259@44"
+    },
+    "n26": {
+        "title": " no cus-pro dependency",
+        "loc": "99@149"
+    },
+    "n31": {
+        "title": " partner fulfill functions",
+        "loc": "616@121"
+    },
+    "n36": {
+        "title": " partner ensure correct usage",
+        "loc": "523@187"
+    },
+    "n41": {
+        "title": " partner as a beta-customer",
+        "loc": "813@122"
+    },
+    "n46": {
+        "title": " provider facing customers",
+        "loc": "1321@120"
+    },
+    "n51": {
+        "title": " partner responsible on quality to providers",
+        "loc": "1213@188"
+    },
+    "n88": {
+        "title": " local host",
+        "loc": "112@63"
+    },
+    "n121": {
+        "title": " cus-par join resp. on quality",
+        "loc": "604@243"
+    },
+    "n126": {
+        "title": " direct code modification",
+        "loc": "419@292"
+    },
+    "n131": {
+        "title": " direct data manipulation",
+        "loc": "496@379"
+    },
+    "n136": {
+        "title": " lightweight development",
+        "loc": "570@335"
+    },
+    "n141": {
+        "title": " consultant development",
+        "loc": "871@312"
+    },
+    "n146": {
+        "title": " joint-test",
+        "loc": "741@320"
+    },
+    "n151": {
+        "title": " provider-side testing",
+        "loc": "1265@295"
+    },
+    "n156": {
+        "title": " postpone upgrade",
+        "loc": "436@465"
+    },
+    "n161": {
+        "title": " reuse and partner app",
+        "loc": "808@411"
+    },
+    "n166": {
+        "title": " code policing",
+        "loc": "1142@418"
+    },
+    "n171": {
+        "title": " minor customisation",
+        "loc": "665@500"
+    },
+    "n180": {
+        "title": " ",
+        "loc": "1000@490"
+    }
+}
+var m = {
+    "n31": [
+        "n7"
+    ],
+    "n36": [
+        "n7"
+    ],
+    "n41": [
+        "n7"
+    ],
+    "n46": [
+        "n17"
+    ],
+    "n26": [
+        "n88"
+    ],
+    "n51": [
+        "n17",
+        "n46"
+    ],
+    "n7": [],
+    "n12": [],
+    "n17": [],
+    "n88": [],
+    "n121": [],
+    "n126": [],
+    "n131": [],
+    "n136": [],
+    "n141": [],
+    "n146": [],
+    "n151": [],
+    "n156": [],
+    "n161": [],
+    "n166": [],
+    "n171": [],
+    "n180": []
+}
